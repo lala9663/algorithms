ebc127f54eff614fba3bd84751e9a3a9e02f1d9f
lala9663 <lala96632040@gmail.com>
2022-10-22 23:48:51 +0900

    feat: 221022

---
+++ programmers/lv0/P04.java
@@ -0,0 +1,9 @@
+package programmers.lv0;
+// 두 수의 곱
+public class P04 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+}
---
+++ programmers/lv0/P06.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+// 각도기
+public class P06 {
+    public int solution(int angle) {
+        int answer = 0;
+        if(angle>0 && angle < 90) {
+            answer = 1;
+        } else if(angle == 90) {
+            answer = 2;
+        } else if(angle < 180) {
+            answer = 3;
+        } else if(angle == 180) {
+            answer = 4;
+        }
+        return answer;
+    }
+}
