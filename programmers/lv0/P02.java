acfa007b8f6134c0ec85d813b381b9a61b2ce08f
lala9663 <lala96632040@gmail.com>
2022-11-05 22:52:00 +0900

    update: 221105

--- programmers/lv0/P02.java
+++ programmers/lv0/P02.java
@@ -4,7 +4,7 @@ public class P02 {
     public int solution(int num1, int num2) {
         double answer = (double) num1 / num2;
         answer = answer * 1000;
-        int y = 1;
+        int z = 2;
         return (int)answer;
     }
 }
