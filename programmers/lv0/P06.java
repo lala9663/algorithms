442964f31be1f0b5dd3b288a8a6b572f75cd9de2
lala9663 <lala96632040@gmail.com>
2022-11-07 21:45:31 +0900

    update: 221107

--- programmers/lv0/P06.java
+++ programmers/lv0/P06.java
@@ -2,7 +2,7 @@ package programmers.lv0;
 // 각도기
 public class P06 {
     public int solution(int angle) {
-        int answer = 0;
+        int answer=1;
         if(angle>0 && angle < 90) {
             answer = 1;
         } else if(angle == 90) {
