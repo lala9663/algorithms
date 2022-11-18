3fda2cbb3a4128c99d90c72370b0c48566ca08f7
taeyunkim <lala96632040@gmail.com>
2022-11-18 22:23:58 +0900

    feat: 221118

--- baekjoon/bronze/B2562.java
+++ baekjoon/bronze/B2562.java
@@ -22,6 +22,6 @@ public class B2562 {
                 index = count;
             }
         }
-        System.out.println(max + "\n" + index);
+        System.out.println(max + "\n " + index);
     }
 }
