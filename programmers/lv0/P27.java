81cbe7cb3bcdf232a44ec76458b4eb6130d21b2a
lala9663 <lala96632040@gmail.com>
2022-11-28 15:01:02 +0900

    update: 221128

---
+++ programmers/lv0/P27.java
@@ -0,0 +1,24 @@
+package programmers.lv0;
+
+public class P27 {
+
+    public int solution(int[] number) {
+        int answer = 0;
+
+        int start = 0;
+        while(start <= number.length-3 ) {
+
+            for(int i = start+1; i< number.length; i++) {
+                for( int j = i+1; j< number.length; j++) {
+                    if(number[start] + number[i] + number[j] == 0) {
+                        answer++;
+                    }
+                }
+            }
+
+            start ++;
+        }
+
+        return answer;
+    }
+}
