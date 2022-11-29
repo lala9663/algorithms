951dbb06da286f621a62cc0eb34ba8611cdd8c00
lala9663 <105348713+lala9663@users.noreply.github.com>
2022-11-29 14:35:00 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        boolean[] arr = new boolean[42];
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+
+        for(int i=0; i<10; i++){
+            arr[Integer.parseInt(br.readLine()) % 42] =true;
+        }
+        int count = 0;
+        for (boolean value : arr){
+            if (value) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
