d9573256eee5d42e25566f862ae6c894d40c039e
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-02-14 00:19:58 +0900

    [Bronze V] Title: 팩토리얼, Time: 124 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = 1;
+
+        while (N != 0) {
+            M = M * N;
+            N--;
+        }
+        System.out.println(M);
+    }
+}
