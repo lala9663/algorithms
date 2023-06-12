a02178a31c6b4ecf4ad8f676e2a5216ee29b2882
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-06-12 17:31:01 +0900

    [Bronze V] Title: 직사각형, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a*b);
+
+    }
+}
