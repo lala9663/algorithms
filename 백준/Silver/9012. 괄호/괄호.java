ddbbb82928590b716799ccde0ce29af36e756a46
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-07-12 02:00:01 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,45 +1,38 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
-
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
-        int a = Integer.parseInt(br.readLine());
+        Stack<Character> stack = new Stack<>(); // 반복문 밖에서 스택 선언
 
-        for (int i = 0; i < a; i++) {
-            sb.append(solve(br.readLine())).append('\n');
-        }
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            boolean isValid = true;
 
-        System.out.println(sb);
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if (ch == '(') {
+                    stack.push(ch);
+                } else if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
 
-    }
-
-    public static String solve(String s) {
-        Stack<String> stack = new Stack<>();
-
-        for (int i = 0; i <s.length() ; i++) {
-            char b = s.charAt(i);
-
-            if (b == '(') {
-                stack.push(Integer.toString(b));
-            } else if (stack.empty()) {
-                return "NO";
+            if (isValid && stack.isEmpty()) {
+                System.out.println("YES");
             } else {
-                stack.pop();
-            }  
-        }
+                System.out.println("NO");
+            }
 
-        if (stack.empty()) {
-            return "YES";
-        } else {
-            return "NO";
-        } 
+            stack.clear(); // 반복문 종료 후 스택 초기화
+        }
     }
 }
