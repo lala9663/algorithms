920f33b312596099dde2109f57ccb0e60b2f0584
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-07-12 16:25:22 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -4,42 +4,42 @@ import java.util.List;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> result = new ArrayList<>();
-        
-        int[] a = new int[progresses.length];  
-        int[] b = new int[progresses.length];  
+
+        int[] a = new int[progresses.length];
+        int[] b = new int[progresses.length];
         int day = 0;
-        
+
         for (int i = 0; i < progresses.length; i++) {
-    a[i] = progresses[i];
-    
-    if ((100 - a[i]) % speeds[i] == 0) {
-        b[i] = (100 - a[i]) / speeds[i];
-    } else {
-        b[i] = (100 - a[i]) / speeds[i] + 1;
-    }
-}
-        
-        int count = 0; 
-        int currentDay = b[0]; 
-        
+            a[i] = progresses[i];
+
+            if ((100 - a[i]) % speeds[i] == 0) {
+                b[i] = (100 - a[i]) / speeds[i];
+            } else {
+                b[i] = (100 - a[i]) / speeds[i] + 1;
+            }
+        }
+
+        int count = 0;
+        int currentDay = b[0];
+
         for (int i = 0; i < progresses.length; i++) {
             if (b[i] <= currentDay) {
                 count++;
             } else {
-                result.add(count); 
+                result.add(count);
                 count = 1;
-                currentDay = b[i]; 
+                currentDay = b[i];
             }
         }
-        
+
         result.add(count);
-        
+
         int[] answer = new int[result.size()];
-        
+
         for (int i = 0; i < result.size(); i++) {
             answer[i] = result.get(i);
         }
-        
+
         return answer;
     }
 }
