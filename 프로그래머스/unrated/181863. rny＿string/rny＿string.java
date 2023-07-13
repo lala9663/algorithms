c35aa53b8b52b49ce25544ed7d11dc927a605ae2
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-07-14 02:03:06 +0900

    [unrated] Title: rny_string, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+
+        if (rny_string.contains("m")) {
+            answer = rny_string.replace("m", "rn");
+        } else {
+            return answer = rny_string;
+        } 
+
+        return answer;
+    }
+}
