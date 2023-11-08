a1409fa42e8b62af176564dacc4b1f7f440d5c42
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-11-08 13:28:43 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,21 +1,18 @@
-    class Solution {
-        public int solution(String my_string, String is_prefix) {
-            int answer = 0;
-            
-
-            for(int i = 0; i < is_prefix.length(); i++){
-                if(my_string.length() < is_prefix.length()){
-                answer = 0;
-                break;
-                }
-                if(my_string.charAt(i)==is_prefix.charAt(i)){
-                    answer = 1;
-                } else {
-                    answer = 0;
-                    break;
-                }
-            }
-
-            return answer;
-        }
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        
+        if (my_string.charAt(0)==is_prefix.charAt(0) && my_string.length() >= is_prefix.length()) {
+        	for (int i = 1; i < is_prefix.length(); i++) {
+				if (my_string.charAt(i)!=is_prefix.charAt(i)) {
+					answer = 0;
+				} else {
+					answer = 1;
+				}
+			}
+		}
+        
+        
+        return answer;
     }
+}
