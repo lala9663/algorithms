e2005b9e4ce037ace65ecc3fb5a39326f0ed77d7
lala9663 <105348713+lala9663@users.noreply.github.com>
2023-11-08 13:51:00 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,27 +1,14 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
+        
+		String result = myString.replace("A", "C").replace("B", "A").replace("C", "B");
 
-        String[] word = myString.split("");
-        String change = "";
-        for (int i = 0; i <word.length ; i++) {
-            if (word[i].equals("A")) {
-                word[i] = "B";
-            } else {
-                word[i] = "A";
-            }
-        }
-
-        for (int i = 0; i < word.length; i++) {
-            change += word[i];
-        }
-
-        if (change.contains(pat)) {
-            answer = 1;
-        } else {
-            answer = 0;
-        }
-
+		
+		if (result.contains(pat)) {
+			answer = 1;
+		}
+        
         return answer;
     }
 }
