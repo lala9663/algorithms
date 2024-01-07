5dee8a263f442cd6d49759690f00f255eae79e75
lala9663 <105348713+lala9663@users.noreply.github.com>
2024-01-08 01:52:37 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void) {
+    int A, B, C;
+    2 <= A;
+    C <= 10000;
+    scanf("%d %d %d", &A, &B, &C);
+    printf("%d\n", (A+B)%C);
+    printf("%d\n", ((A%C) + (B%C))%C);
+    printf("%d\n", (A*B)%C);
+    printf("%d\n", ((A%C) * (B%C))%C);
+}
